6ce9ee31f68f6a8d1ef78e7d84c981ec19500e19
RajesRana84 <raj.infypune@gmail.com>
2018-11-21 23:35:30 +0530

    Feign Client added

--- currency-conversion-service/src/main/java/com/project/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/project/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,19 +1,57 @@
 package com.project.microservices.currencyconversionservice;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
 	
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
 		
-		return new CurrencyConversionBean(1l,from,to,BigDecimal.ONE,quantity,quantity,0);
+		// Using Rest Template to Invoke other/external rest Service
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+
+		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,
+				uriVariables);
+
+		CurrencyConversionBean response = responseEntity.getBody();
+		
+		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
+				quantity.multiply(response.getConversionMultiple()), response.getPort());
 	}
 
+	// Using Feign Client to Invoke External Rest Calls
+	// 1 - Add Feign Dependency in pom file
+	// 2 - ENable feign client - @EnableFeignClients
+	// 3 - Create Feign Proxy
+	// 4 - Autowire Feign Proxy in your class and use it to invoke rest calls
+	
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+
+		//logger.info("{}", response);
+		
+		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
+				quantity.multiply(response.getConversionMultiple()), response.getPort());
+	}
 }

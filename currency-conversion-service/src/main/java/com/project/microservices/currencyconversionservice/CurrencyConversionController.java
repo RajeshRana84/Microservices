f12d7aa64c34b38f7815d5d3388c85f003a6609a
RajeshRana84 <raj.infypune@gmail.com>
2018-11-23 18:41:30 +0530

    from office - Sleuth Added

--- currency-conversion-service/src/main/java/com/project/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/project/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,8 +4,9 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,8 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private CurrencyExchangeServiceProxy proxy;
@@ -49,7 +52,7 @@ public class CurrencyConversionController {
 
 		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
 
-		//logger.info("{}", response);
+		logger.info("{}", response);
 		
 		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
 				quantity.multiply(response.getConversionMultiple()), response.getPort());
